908ec3fda417b17f1c03c8b2f4058c90ba49b107
FolorunshoAyo <folushoayomide11@gmail.com>
2021-09-18 21:13:49 +0100

    added all files

---
+++ script.js
@@ -0,0 +1,48 @@
+(function () {
+    const ballons = document.querySelectorAll(".ballon");
+    let popCount = 0;
+    const generateHexValue = () => {
+        let hexValue = "";
+        let hexData = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
+        for (let i = 0; i < 6; i++) {
+            let randomIndex = Math.floor(Math.random() * 15);
+            hexValue += hexData[randomIndex];
+        }
+        return hexValue;
+    };
+
+    colorBubbles();
+
+    function colorBubbles() {
+        ballons.forEach(ballon => {
+            let hexValue = "#" + generateHexValue();
+            ballon.style.backgroundColor = hexValue;
+        });
+    }
+
+    function popOnHover() {
+        ballons.forEach(ballon => {
+            ballon.addEventListener("mouseover", popBallon, false);
+        });
+    }
+
+    function popBallon(){
+        let backgroundColor = this.style.backgroundColor;
+
+        this.style.borderRadius = "0";
+        this.style.backgroundColor = "initial";
+        this.textContent = "POP!";
+        this.style.color = backgroundColor;
+
+        popCount++;
+        this.removeEventListener("mouseover", popBallon, false);
+        if (popCount == 25) {
+            setTimeout(() => {
+                alert("Great Job");
+                setTimeout(() => confirm("restart?") ? location.reload() : window.close(), 1000);
+            }, 1000);
+        }
+    }
+
+    popOnHover();
+}());
